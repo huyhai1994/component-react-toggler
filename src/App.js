f3494fd56eb4c30e02251dccf71c76cb40b2adfd
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-18 14:16:17 +0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import '/bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import {Component} from "react";
 
 
-function App() {
-    return (
-        <div className="App">
+class App extends Component() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isExpand: false
+        }
+    }
+
+    render() {
+        return <div className="App">
         </div>
-    );
+    }
 }
 
 export default App;
