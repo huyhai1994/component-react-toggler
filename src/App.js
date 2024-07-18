afd10f36c2062fc8705cbe53a7e669a3b43b13ad
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-18 14:54:03 +0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import './App.css';
-import '/bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import {Component} from "react";
+import React, {Component} from "react";
 
-
-class App extends Component() {
+class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             isExpand: false
-        }
+        };
+    }
+
+    toggleText = () => {
+        let previousState = this.state.isExpand;
+        this.setState({isExpand: !previousState});
     }
 
     render() {
-        return <div className="App">
-        </div>
+        let message;
+        if (this.state.isExpand) {
+            message = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
+        }
+        return (<div>
+            <button type='button'
+                    onClick={this.toggleText}> Xem giới thiệu
+            </button>
+            <h1>{message}</h1>
+        </div>)
+            ;
     }
 }
 
